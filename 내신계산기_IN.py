d15cb19dd21e0f6c54246d0398473979ff0054bc
polydou2264 <happykevin13953@gmail.com>
2021-07-12 20:37:38 +0900

    최종완성!!

--- 내신계산기_IN.py
+++ 내신계산기_IN.py
@@ -11,7 +11,7 @@ def zp(a,b):
 
 def zd(a,b):
     m = 100 * (a-20+b) / a
-    return max
+    return m
 
 print("1: 지필 → 원점수, 2: 수행 → 원점수, 3: 수행 세부점수 → 수행 → 원점수, 4: 수행 → 절대 컷, 5: 모든 점수 합산 → 원점수")
 switch = int(input("모드를 입력하세요: "))
@@ -71,7 +71,7 @@ elif switch == 3: #3: 수행 세부점수 → 수행 → 원점수
 
 elif switch == 4: #4: 수행 → 절대 컷
     print("<수행 → 절대 컷> 모드를 선택하셨습니다.") 
-    sh_1=[]
+
     n = int(input("수행평가의 개수를 입력하세요:"))
     print(" ")
 
@@ -93,11 +93,42 @@ elif switch == 4: #4: 수행 → 절대 컷
     
     cnt = cnt_ts - cnt
 
-    print(cnt)
-
-    print("당신의 절대 지필 컷 점수는 ", zd(j ,cnt), "입니다.")
+    print("당신의 절대 지필 컷 점수는", int(zd(j ,cnt)), "입니다.")
 
 elif switch == 5: #5: 모든 점수 합산 → 원점수
-    print("<모든 점수 합산 → 원점수> 모드를 선택하셨습니다.")   
+    print("<모든 점수 합산 → 원점수> 모드를 선택하셨습니다.")
+
+    n = int(input("수행평가의 개수를 입력하세요:"))
+    print(" ")
+
+    i=0
+    cnt = 0
+    cnt_ts = 0;
+
+    while n>0:
+        print(i+1,"번째 수행점수(원점수X)를 입력하세요:", end='')
+        k = int(input())
+        print(i+1,"번째 수행 비율을 입력하세요:", end='')
+        g = int(input())
+        cnt_ts = cnt_ts + g
+        cnt = cnt + sh(k,g)
+        n = n - 1
+        i = i + 1
+        print(" ")
+    
+    pt_pc = (100 - cnt_ts) / 2
+
+    print("지필 점수를 입력해주세요(중간 기말) EX) 90 85")
+
+    test_1, test_2 = input().split()
+    test_1 = int(test_1)
+    test_2 = int(test_2)
+    print(" ")
+
+
+    tot = zp(test_1, pt_pc) + zp(test_2, pt_pc) + cnt
+
+    print("당신의 원점수는 ",tot,"입니다.")
+    
 
 
